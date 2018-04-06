782e32d5ac309900f4c5f1bd0f96247b8ce144d9
Dalton Schnieders <dschnieders@ksu.edu>
2018-04-05 22:23:21 -0500

    modified search.html

--- public/searchAction.js
+++ public/searchAction.js
@@ -1,12 +1,5 @@
 const igdb = require('igdb-api-node').default;
 const client = igdb('d24a990ec0ea072538e13060eda5b3ae');
 
-client.games({
-    fields: '*', // Return all fields
-    limit: 5, // Limit to 5 results
-    offset: 15 // Index offset for results
-}).then(response => {
-    // response.body contains the parsed JSON response to this query
-}).catch(error => {
-    throw error;
-});
+/// How to get input from user and then search via the API for the data on 
+/// the specified game???
