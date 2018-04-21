cb8897797e528c588b0671e8e8caf440d7f864b2
Dalton Schnieders <dschnieders@ksu.edu>
2018-04-21 16:05:23 -0500

    added gamePage.html and gamePage.css

--- public/searchAction.js
+++ public/searchAction.js
@@ -14,34 +14,16 @@ document.querySelector('form.search').addEventListener('submit', function (e) {
         var searchResults = document.querySelector("#searchResults");
         searchResults.innerHTML = "";
         for(var i = 0; i < results.length; i++){
+            /*var cover = document.createElement("img");
+            cover.src = client.image({
+                cloudinary_id: results[i].id
+            }, 'micro');*/
             var node = document.createElement("a");
             node.text = results[i].name;
             node.href = "/games/" + results[i].id;
             searchResults.appendChild(node);
+            var br = document.createElement("br");
+            searchResults.appendChild(br);
         }
     });
 });
-
-/// How to get input from user and then search via the API for the data on 
-/// the specified game???
-
-/// I think the following is the endpoint that we will end up using...?
-
-/*
-    Search for up to [limit] [user_input] games
-    Can filter it by release date, rating, etc
-    Can sort it be release date, rating, etc
-*/
-
-/* client.games({
-    limit: 20,              // limits the amount of results to 20 (default is 10, max = 50)
-    offset: 0,              // offset will start a position [i] and give the [limit] results after that
-    search: 'user_input'    // search with the input given by the user
-}, [
-    'name',                 // the game's name
-    'release_dates.date',   // the game's initial release date
-    'rating',               // average user rating
-    'summary',              // summary of the game
-    'cover'                 // image object reference, see  https://igdb.github.io/api/misc-objects/image/ 
-                            // can access the url for the image if we want to include that somehow
-]).then(log); */
