401b627a4e3114165894cd516c9fbf17d2119196
smalhi5 <simmalhi123@gmail.com>
2018-04-25 12:49:04 -0500

    images

--- public/searchAction.js
+++ public/searchAction.js
@@ -21,27 +21,3 @@ document.querySelector('form.search').addEventListener('submit', function (e) {
         }
     });
 });
-
-/// How to get input from user and then search via the API for the data on 
-/// the specified game???
-
-/// I think the following is the endpoint that we will end up using...?
-
-/*
-    Search for up to [limit] [user_input] games
-    Can filter it by release date, rating, etc
-    Can sort it be release date, rating, etc
-*/
-
-/* client.games({
-    limit: 20,              // limits the amount of results to 20 (default is 10, max = 50)
-    offset: 0,              // offset will start a position [i] and give the [limit] results after that
-    search: 'user_input'    // search with the input given by the user
-}, [
-    'name',                 // the game's name
-    'release_dates.date',   // the game's initial release date
-    'rating',               // average user rating
-    'summary',              // summary of the game
-    'cover'                 // image object reference, see  https://igdb.github.io/api/misc-objects/image/ 
-                            // can access the url for the image if we want to include that somehow
-]).then(log); */
--- server.js
+++ server.js
@@ -24,7 +24,23 @@ app.use(express.static("./public"));
 app.get('/', function(req, res) {
     res.sendfile('public/search.html'); 
 });
+
 app.get('/games', function(req, res) {
+  return client.games({
+      fields: '*',
+      limit: 20,
+      offset: 0,
+      search: req.query.text
+  }).then(igdbResponse => {
+    res.send(igdbResponse.body);
+  }); 
+});
+
+app.get('/games', function(req, res) {
+  client.image({
+    cloudinary_id: res.peram.id;
+  }, 'micro', 'jpg');
+
   return client.games({
       fields: '*',
       limit: 20,
