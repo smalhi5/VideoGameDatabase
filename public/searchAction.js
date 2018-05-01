1980c441909cfa056508cd8e34707c14d586a4d8
Simran Malhi <simmalhi123@gmail.com>
2018-05-01 17:45:41 -0500

    fixed the image problem

--- public/searchAction.js
+++ public/searchAction.js
@@ -16,9 +16,10 @@ document.querySelector('form.search').addEventListener('submit', function (e) {
         for(var i = 0; i < results.length; i++){
             var li = document.createElement("li");
             var img = document.createElement("img");
-            img.src = results[i].cover.url;
-            if (!results[i].cover.url){
+            if (!results[i]["cover"]){
                 img.src = "https://dummyimage.com/90x90/000/0011ff.jpg&text=no+image+available+";
+            }else{
+                img.src = results[i].cover.url;
             }
             var node = document.createElement("a");
             li.appendChild(node);
